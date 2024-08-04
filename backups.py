c9edd89db57c8cd0f97e0e834d79bd3ac3623e5f
Ondrej Novak <ondrej@npost.cz>
2024-08-04 17:13:15 +0200

    Allow to easily source repo env for testing

--- backups.py
+++ backups.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import json
+import logging
 import os
 import subprocess
 import sys
@@ -10,15 +11,19 @@ from pathlib import Path
 
 MIN_SNAPSHOTS_PER_WEEK = 5
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, stream=sys.stderr)
+
 def usage():
     file = sys.argv[0]
     print(f"""Usage: {file} [OPERATION] [REPO_CONFIG_FILE] [PASSWORD_FILE] [SOURCE_CONFIG_FILE?]
 Backs up the files given by the source configuration file to the repository given by the repo configuration file. Password file contains repo password.
 
 Operations:
-    backup      Creates a snapshot. Requires 3 more arguments: repo config, password file, source config.
-    init        Initializes the repository. Requires 2 more arguments: repo config, password file.
-    verify      Verifies the repo consistency and checks if there is at least 5 snapshots taken last week. Requires 2 arguments: repo config, password file.
+    backup              Creates a snapshot. Requires 3 more arguments: repo config, password file, source config.
+    init                Initializes the repository. Requires 2 more arguments: repo config, password file.
+    verify              Verifies the repo consistency and checks if there is at least 5 snapshots taken last week. Requires 2 arguments: repo config, password file.
+    compile-dotenv      Prints the ENV variables from the repo config file in format that can be `source`d. Requires 1 argument: repo config.
 
 Example:
     {file} /path/to/source/config.json /path/to/repo/config.json /path/to/password/file
@@ -74,15 +79,15 @@ def _execute(command: str, repo_config, capture_output: bool = False) -> str:
 
 
 def backup(repo: Path, pwfile: Path, src: Path):
-    print(f"Reading config: {src}")
+    logger.info(f"Reading config: {src}")
     src_config = json.loads(src.read_text())
 
-    print(f"Reading config: {repo}")
+    logger.info(f"Reading config: {repo}")
     repo_config = json.loads(repo.read_text())
 
-    print(f"Backing up files to {repo_config["url"]}")
+    logger.info(f"Backing up files to {repo_config["url"]}")
     for dir in src_config["dirs"]:
-        print(f"- {dir}")
+        logger.info(f"- {dir}")
     
     dirs = " ".join(src_config["dirs"])
     excludes = " ".join([f"--exclude {ex}" for ex in src_config.get("exclude", [])])
@@ -92,7 +97,7 @@ def backup(repo: Path, pwfile: Path, src: Path):
 
 
 def init(repo: Path, pwfile: Path):
-    print(f"Reading config: {repo}")
+    logger.info(f"Reading config: {repo}")
     repo_config = json.loads(repo.read_text())
 
     cmd = f"restic -r {repo_config["url"]} init --password-file {pwfile}"
@@ -100,16 +105,16 @@ def init(repo: Path, pwfile: Path):
 
 
 def verify(repo: Path, pwfile: Path):
-    print(f"Reading config: {repo}")
+    logger.info(f"Reading config: {repo}")
     repo_config = json.loads(repo.read_text())
 
     cmd = f"restic -r {repo_config["url"]} check --read-data-subset=5% --password-file {pwfile}"
     
     try:
         _execute(cmd, repo_config)
-        print("Repo consistency OK")
+        logger.info("Repo consistency OK")
     except:
-        print("Repo consistency check failed")
+        logger.error("Repo consistency check failed")
         return
     
 
@@ -120,26 +125,32 @@ def verify(repo: Path, pwfile: Path):
     backups_ok = True
 
     for group in groups:
-        print(f"Group: {' '.join(group['group_key']['paths'])}")
+        logger.info(f"Group: {' '.join(group['group_key']['paths'])}")
 
         timestamps = [datetime.fromisoformat(snapshot["time"]) for snapshot in group["snapshots"]]
         last_week_snapshots = [timestamp for timestamp in timestamps if datetime.now(timezone.utc) - timestamp < timedelta(days=7)]
-        print(f"Last week snapshot count: {len(last_week_snapshots)}")
+        logger.info(f"Last week snapshot count: {len(last_week_snapshots)}")
         if len(last_week_snapshots) < MIN_SNAPSHOTS_PER_WEEK:
-            print("Insufficient number of snapshots!")
+            logger.error("Insufficient number of snapshots!")
             backups_ok = False
 
     if not backups_ok:
-        print("Not enough snapshots taken!")
+        logger.info("Not enough snapshots taken!")
         sys.exit(1)
+
+
+def compile_dotenv(repo: Path):
+    logger.info(f"Reading config: {repo}")
+    repo_config = json.loads(repo.read_text())
+
+    logger.info(f"In bash, you can source by running: source <(./backups.py compile-dotenv {str(repo)})")
+    for name, value in repo_config.get("env", {}).items():
+        print(f"export {name}={value}")
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
         usage()
-        sys.exit(1)
-    elif sys.argv[1] not in {"backup", "init", "verify"}:
-        print(f"Unknown argument: {sys.argv[1]}")
         sys.exit(1)
     elif sys.argv[1] == "backup":
         assert len(sys.argv) == 5, "`backup` needs 3 arguments"
@@ -150,4 +161,9 @@ if __name__ == "__main__":
     elif sys.argv[1] == "verify":
         assert len(sys.argv) == 4, "`verify` needs 2 arguments"
         verify(Path(sys.argv[2]), Path(sys.argv[3]))
-
+    elif sys.argv[1] == "compile-dotenv": # For debugging
+        assert len(sys.argv) == 3, "`compile-dotenv` needs 1 argument"
+        compile_dotenv(Path(sys.argv[2]))
+    else:
+        print(f"Unknown argument: {sys.argv[1]}")
+        sys.exit(1)
